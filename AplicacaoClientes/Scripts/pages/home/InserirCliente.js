89cfdeebe8e7c057228ccfebab8ef15f9b2b81cd
Felipe <felipe.monteiro2626@gmail.com>
2024-05-17 21:11:15 -0300

    feat : envio de formulario via java script

--- AplicacaoClientes/Scripts/pages/home/InserirCliente.js
+++ AplicacaoClientes/Scripts/pages/home/InserirCliente.js
@@ -1,7 +1,22 @@
 ﻿$(document).ready(function () {
 
-    $("#salvarUser").on("click", function () {
-        alert("clicou")
+    //$("#salvarUser").on("click", function () {
+    //    alert("clicou")
+    //});
+    $("#form-insert-user").submit(function (e) {
+        e.preventDefault();
+        var formAction = $(this).attr("action");
+        var fdata = $(this).serialize();;
+
+        console.log("Dados codificados: " + fdata);
+
+        // Para ver os dados em formato legível
+        var decodedData = decodeURIComponent(fdata);
+        console.log("Dados decodificados: " + decodedData);
+        var nome = $("#nome").val();
+        var email = $("#email").val();
+        console.log("Nome: " + nome);
+        console.log("Email: " + email);
     });
     $(".close").on("click", function () {
         $("#modalDialog").modal('hide');
